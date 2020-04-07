c7b838289bf1d2aa29caaabe2376d718d4009c1c
Manish Munikar <munikarmanish@gmail.com>
2020-04-06 20:19:30 -0500

    disjoint-set: path-compression and union-by-rank

--- data_structures/disjointset.py
+++ data_structures/disjointset.py
@@ -10,28 +10,36 @@ class DisjointSet:
     Disjoint set of items.
     """
 
-    def __init__(self, items=None, parents=None):
+    def __init__(self, items=None):
         self.items = [] if items is None else items
-        self.parents = parents
-        if parents is None:
-            self.parents = {item: None for item in items}
+        self.parents = {item: None for item in items}
+        self.ranks = {item: 1 for item in items}
 
     def find(self, item):
         """
-        Find the set representative.
+        Find the set representative. (Uses path-compression)
         """
         if self.parents[item] is None:
             return item
-        return self.find(self.parents[item])
+        self.parents[item] = self.find(self.parents[item])
+        return self.parents[item]
 
     def union(self, a, b):
         """
-        Merge two sets (if not already merged).
+        Merge two sets if not already merged. (union-by-rank)
         """
         a_root = self.find(a)
         b_root = self.find(b)
-        if a_root != b_root:
+
+        if a_root == b_root:
+            return
+
+        if self.ranks[a_root] < self.ranks[b_root]:
+            self.parents[a_root] = b_root
+        else:
             self.parents[b_root] = a_root
+            if self.ranks[a_root] == self.ranks[b_root]:
+                self.ranks[a_root] += 1
 
     def __repr__(self):
         sets = defaultdict(set)
